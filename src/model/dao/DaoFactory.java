8e25824b337199ead665c93dfdc8c9478a426980
Victor Oliveira <victor.oliveira20@icloud.com>
2023-07-31 16:37:52 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,12 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+	
+	public static SellerDAO createSellerDao() {
+		
+		return new SellerDaoJDBC();
+		
+	}
+}
