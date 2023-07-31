c476f2d1dacd69aab4f710246ac30c59d8ad7ad6
Victor Oliveira <victor.oliveira20@icloud.com>
2023-07-31 17:53:42 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -36,6 +37,13 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n==== TEST 4: seller findAll ====");
+		
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		
+		System.out.println("Inserted! new Id = " + newSeller.getId());
 		
 	}
 
