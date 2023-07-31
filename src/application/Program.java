f430eaabc6d42111ec2c8da1be4a63016f7f1d31
Victor Oliveira <victor.oliveira20@icloud.com>
2023-07-31 18:00:42 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,12 +38,18 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n==== TEST 4: seller findAll ====");
+		System.out.println("\n==== TEST 4: seller Insert ====");
 		
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		
 		System.out.println("Inserted! new Id = " + newSeller.getId());
+		
+		System.out.println("\n==== TEST 5: seller Update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 		
 	}
 
