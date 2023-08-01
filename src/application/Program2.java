4db8581154b2342318c9912509fe76bb40d16132
Victor Oliveira <victor.oliveira20@icloud.com>
2023-08-01 11:25:43 -0300

    Department findById implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.Scanner;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDAO;
 import model.entities.Department;
@@ -8,6 +10,7 @@ public class Program2 {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		DepartmentDAO departmentDao = DaoFactory.createDepartmentDao();
 		
 		System.out.println("\n==== TEST 1: Department Insert ====");
@@ -17,7 +20,20 @@ public class Program2 {
 		departmentDao.insert(dep);
 		
 		System.out.println("Novo departamento gerado: " + dep);
-
+		
+		
+		System.out.println("\n==== TEST 2: Department Update ====");
+		dep = departmentDao.findById(1);
+		dep.setName("Food");		
+		departmentDao.update(dep);
+		System.out.println("Update realizado!");
+		
+		
+		System.out.println("\n==== TEST 3: Department findById ====");
+		dep = departmentDao.findById(3);
+		
+		System.out.println(dep);
+		
 	}
 
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -11,6 +11,7 @@ import db.DB;
 import db.DbException;
 import model.dao.DepartmentDAO;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class DepartmentDaoJDBC implements DepartmentDAO {
 
@@ -27,7 +28,6 @@ public class DepartmentDaoJDBC implements DepartmentDAO {
 			st = conn.prepareStatement("INSERT INTO department " + "(Name) " + "VALUES " + "(?)",
 					Statement.RETURN_GENERATED_KEYS);
 
-			
 			st.setString(1, obj.getName());
 
 			int rowsAffected = st.executeUpdate();
@@ -53,7 +53,19 @@ public class DepartmentDaoJDBC implements DepartmentDAO {
 
 	@Override
 	public void update(Department obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("UPDATE department SET Name = ? WHERE Id = ?", Statement.RETURN_GENERATED_KEYS);
+
+			st.setString(1, obj.getName());
+			st.setInt(2, obj.getId());
+
+			st.executeUpdate();
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 
 	}
 
@@ -65,10 +77,39 @@ public class DepartmentDaoJDBC implements DepartmentDAO {
 
 	@Override
 	public Department findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement("SELECT department.* "
+					+ "FROM department "
+					+ "WHERE department.Id = ? " 
+					+ "ORDER BY Name", Statement.RETURN_GENERATED_KEYS);
+			
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			
+			if(rs.next()) {
+				int departmentId = rs.getInt("Id");
+				String departmentName = rs.getString("Name");
+				
+				Department department = new Department(departmentId, departmentName);
+				return department;
+			}
+			
+			return null;
+			
+		}
+		catch(SQLException e){
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
+	
 	@Override
 	public List<Department> findAll() {
 		// TODO Auto-generated method stub
